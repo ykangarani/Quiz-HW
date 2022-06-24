078a5c47b99a5be912a6319ea8f594080d69ecc8
ykangarani <kangarani.yousef1991@gmail.com>
2022-06-24 15:45:33 -0700

    updated

--- highscores.js
+++ highscores.js
@@ -1,7 +1,27 @@
-const highScoresList = document.querySelector('#highScoresList')
-const highScores = JSON.parse(localStorage.getItem('highScores')) || []
+var highScore = document.querySelector("#highScore");
+var clear = document.querySelector("#clear");
+var goBack = document.querySelector("#goBack");
 
-highScoresList.innerHTML =
-highScore.map(score => {
-    return `<li class"high-score">${score.name} - ${score.score}</li>`
-}).join('')
+// Event listener to clear scores 
+clear.addEventListener("click", function () {
+    localStorage.clear();
+    location.reload();
+});
+// Retreives local stroage 
+var allScores = localStorage.getItem("allScores");
+allScores = JSON.parse(allScores);
+
+if (allScores !== null) {
+
+    for (var i = 0; i < allScores.length; i++) {
+
+        var createLi = document.createElement("li");
+        createLi.textContent = allScores[i].initials + " " + allScores[i].score;
+        highScore.appendChild(createLi);
+
+    }
+}
+// Event listener to move to index page
+goBack.addEventListener("click", function () {
+    window.location.replace("./index.html");
+});
